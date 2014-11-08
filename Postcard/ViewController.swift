e9a54c4a6d2009054dcab46a2cc75b8731d2a0ea
David Adam <david.g.adam@gmail.com>
2014-11-08 15:17:43 +0800

    Connect to IBOutlets and add IBOutlet actions.

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameFextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+    }
 
 }
 
